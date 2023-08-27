fb6303aa6251f370a2c6fe1ec16197a46a94a917
Marcio Carvalho <marweck@gmail.com>
2023-08-27 13:10:21 -0700

    Fixes graalvm native compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.2"
+    id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.graalvm.buildtools.native") version "0.9.24"
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.spring") version "1.9.0"
-    kotlin("plugin.jpa") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
+    kotlin("plugin.jpa") version "1.9.10"
 }
 
 group = "com.hugomarques"
@@ -18,18 +18,7 @@ repositories {
 }
 
 java {
-    // This project uses Java 21 apis
     sourceCompatibility = JavaVersion.VERSION_20
-    targetCompatibility = JavaVersion.VERSION_20
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        // Null-safety for Spring API in Kotlin
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        // Highest so far Java version supported by Kotlin
-        jvmTarget = "20"
-    }
 }
 
 dependencies {
@@ -39,13 +28,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.gatling.highcharts:gatling-charts-highcharts:3.9.5")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        // Null-safety for Spring API in Kotlin
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        // Highest so far Java version supported by Kotlin
+        jvmTarget = "20"
+    }
 }
 
 tasks.withType<Test> {
